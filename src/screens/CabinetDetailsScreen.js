7b8a5cf5d9b726624e05df0345957fea2cd18174
edavetisyan <edmondavetisyanw@gmail.com>
2022-07-20 16:26:27 +0300

    feat: add modal for details changing

--- src/screens/CabinetDetailsScreen.js
+++ src/screens/CabinetDetailsScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 import {connect} from 'react-redux';
 import BaseApplicationScreen from '../BaseComponents/BaseAppliocationScreen';
 import {cabinetFormChange} from '../core/actions/CabinetActions';
 import {appThemeSelector} from '../core/selectors/AppThemeSelectors';
+import {cabinetDetailsFormSelector} from '../core/selectors/CabinetSelectors';
 import FormService from '../services/FormService';
 import CabinetForm from './components/CabinetForm';
 
@@ -19,19 +26,32 @@ class CabinetDetailsScreen extends BaseApplicationScreen {
     return this.formService.getCabinetForm(cabinetDetailsForm);
   }
   renderForm() {
-    const {navigation} = this.props;
+    const {navigation, cabinetDetailsForm} = this.props;
     return Object.values(this.getForm()).map(item => {
       return (
-        <CabinetForm item={item} key={item.index} navigation={navigation} />
+        <CabinetForm
+          item={item}
+          key={item.index}
+          navigation={navigation}
+          cabinetDetailsForm={cabinetDetailsForm}
+        />
       );
     });
   }
   render() {
-    console.log(this.props);
     const {appTheme} = this.props;
     return (
       <View style={{...styles.root, backgroundColor: appTheme.gray.gray_2}}>
         <View style={styles.inputs_root}>{this.renderForm()}</View>
+        <TouchableHighlight
+          style={{
+            ...styles.save_button,
+            backgroundColor: appTheme.orange.orange_4,
+          }}>
+          <Text style={{...styles.save_text, color: appTheme.gray.gray_1}}>
+            Save
+          </Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -41,17 +61,31 @@ const styles = StyleSheet.create({
     width: width,
     height: height,
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
     paddingTop: 15,
+    paddingBottom: height * 0.15,
   },
   inputs_root: {
     height: height * 0.3,
     justifyContent: 'space-between',
   },
+  save_button: {
+    height: 60,
+    width: width * 0.8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+  save_text: {
+    fontSize: 18,
+    fontWeight: '700',
+    letterSpacing: 7,
+  },
 });
 const mapStateToProps = state => {
   return {
     appTheme: appThemeSelector(state),
+    cabinetDetailsForm: cabinetDetailsFormSelector(state),
   };
 };
 const mapDispatchToProps = dispacth => {
